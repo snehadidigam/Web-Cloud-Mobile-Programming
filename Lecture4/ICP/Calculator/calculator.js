a288e0a8325ab64d02a23ad1ece96850f7a74fad
snehadidigam <snehadidigam@gmail.com>
2018-02-13 11:12:56 -0600

    lec 4

--- Lecture4/ICP/Calculator/calculator.js
+++ Lecture4/ICP/Calculator/calculator.js
@@ -1,4 +1,22 @@
 angular.module('CalculatorApp', [])
     .controller('CalculatorController', function ($scope) {
         // Write code simple calculator operations
+
+        $scope.result = function() {
+            if ($scope.operator == '+') {
+                return $scope.a + $scope.b;
+            }
+            if ($scope.operator == '-') {
+                return $scope.a - $scope.b;
+            }
+            if ($scope.operator == '*') {
+                return $scope.a * $scope.b;
+            }
+            if ($scope.operator == '/') {
+                return $scope.a / $scope.b;
+            }
+            
+        };
+
     });
+    
